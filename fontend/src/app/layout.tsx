1fb17de05c94a163a68c5519dc99ada88533ba80
Jair Oliveira (Ceifeirocv) <jair.almeida.oliveira@gmail.com>
2024-07-03 14:03:10 -0100

    chore: nextjs install

---
+++ fontend/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Recepies Shopping List",
+  description:
+    "Find recipes and generate a shopping list for your weekly menu helping you reducing waste and improving your savings still saving you time.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
