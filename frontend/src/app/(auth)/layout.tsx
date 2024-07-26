2fc51e9d75e5e4d6467a3c746f922ee9b4975b59
Jair Oliveira (Ceifeirocv) <jair.almeida.oliveira@gmail.com>
2024-07-26 11:27:46 -0100

    create side bar

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import {
+  HomeIcon,
+  ReaderIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  CalendarIcon,
+} from "@radix-ui/react-icons";
+import SideNavbar from "@/components/Layout/navbbar/SideNavbar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-full min-h-screen flex-row py-14">
+      <div className="flex flex-col gap-16 w-1/6 h-full px-4 ">
+        <h1 className="text-4xl font-medium">Logo</h1>
+        <SideNavbar />
+      </div>
+      {children}
+    </div>
+  );
+}
---
+++ frontend/src/components/Layout/navbbar/SideNavbar.tsx
@@ -0,0 +1,51 @@
+import {
+  BookmarkIcon,
+  CalendarIcon,
+  ClipboardIcon,
+  HomeIcon,
+  ReaderIcon,
+} from "@radix-ui/react-icons";
+import Link from "next/link";
+import React from "react";
+
+export default function SideNavbar() {
+  return (
+    <nav className="flex flex-col items-start px-2 text-sm font-medium lg:px-4 gap-9 ">
+      <Link
+        href="#"
+        className="flex items-center justify-start gap-2 h-10 w-full rounded-sm px-3 py-2  transition-all hover:bg-[#E4F0DB] hover:text-primary"
+      >
+        <HomeIcon />
+        Home
+      </Link>
+      <Link
+        href="#"
+        className="flex items-center justify-start gap-2 h-10 w-full rounded-sm px-3 py-2  transition-all hover:bg-[#E4F0DB] hover:text-primary bg-primary-foreground"
+      >
+        <ReaderIcon />
+        Recipes
+      </Link>
+      <Link
+        href="#"
+        className="flex items-center justify-start gap-2 h-10 w-full rounded-sm px-3 py-2  transition-all hover:bg-[#E4F0DB] hover:text-primary"
+      >
+        <ClipboardIcon />
+        Shopping List
+      </Link>
+      <Link
+        href="#"
+        className="flex items-center justify-start gap-2 h-10 w-full rounded-sm px-3 py-2  transition-all hover:bg-[#E4F0DB] hover:text-primary"
+      >
+        <CalendarIcon />
+        Meal Planner
+      </Link>
+      <Link
+        href="#"
+        className="flex items-center justify-start gap-2 h-10 w-full rounded-sm px-3 py-2  transition-all hover:bg-[#E4F0DB] hover:text-primary"
+      >
+        <BookmarkIcon />
+        Favorites
+      </Link>
+    </nav>
+  );
+}
