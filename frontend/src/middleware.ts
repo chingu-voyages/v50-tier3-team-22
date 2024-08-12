d3d96c3982417299c828b39d2f581afadab7543b
Jair Oliveira (Ceifeirocv) <jair.almeida.oliveira@gmail.com>
2024-08-12 08:48:09 -0100

    fix: redirect url

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,18 @@
-import {NextRequest, NextResponse} from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware (request:NextRequest){
-    const url= request.nextUrl.clone()
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
 
-    // check for token
-    const token = request.cookies.get("auth_token")
+  // check for token
+  const token = request.cookies.get("auth_token");
 
-    if(!token){
-        url.pathname = "/login"
-        return NextResponse.redirect('/url')
-    }
+  if (!token) {
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
 
-    // progress if authenticated
-    return NextResponse.next()
+  // progress if authenticated
+  return NextResponse.next();
 }
 
-export const config = {matcher:["/:path*"]}
+export const config = { matcher: ["/:path*"] };
